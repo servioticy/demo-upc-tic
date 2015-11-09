0a7758acd2ece241c563cdae31a9deeb2dd4c552
jlupox <jlupox@gmail.com>
2015-11-09 23:44:25 +0100

    correct sos

--- delete_all_sos.py
+++ delete_all_sos.py
@@ -5,7 +5,6 @@ import json
 import requests
 
 def check_resp(resp):
-    import ipdb; ipdb.set_trace() # BREAKPOINT
     if resp.status_code != 204:
         print "Error deleting the SO"
         print 'resp.status_code = ' + str(resp.status_code)
@@ -28,17 +27,17 @@ headers['Authorization'] = 'NDU0NDg1NzktNzkwZS00NGI3LTkyNWQtMDQwMWQ1ODEyM2M3ZGU0
 
 # print ids
 
-import ipdb; ipdb.set_trace() # BREAKPOINT
-# Delete Sensors
-print ids['photocell']
-resp = requests.delete('http://api.servioticy.com/' + ids['photocell'], headers=headers)
-check_resp(resp)
-print ids['tempsensor']
-resp = requests.delete('http://api.servioticy.com/' + ids['tempsensor'], headers=headers)
-check_resp(resp)
-print ids['humiditysensor']
-resp = requests.delete('http://api.servioticy.com/' + ids['humiditysensor'], headers=headers)
-check_resp(resp)
+# import ipdb; ipdb.set_trace() # BREAKPOINT
+# # Delete Sensors
+# print ids['photocell']
+# resp = requests.delete('http://api.servioticy.com/' + ids['photocell'], headers=headers)
+# check_resp(resp)
+# print ids['tempsensor']
+# resp = requests.delete('http://api.servioticy.com/' + ids['tempsensor'], headers=headers)
+# check_resp(resp)
+# print ids['humiditysensor']
+# resp = requests.delete('http://api.servioticy.com/' + ids['humiditysensor'], headers=headers)
+# check_resp(resp)
 
 # Delete elements
 import ipdb; ipdb.set_trace() # BREAKPOINT
@@ -49,6 +48,18 @@ for key, value in ids.iteritems():
         resp = requests.delete('http://api.servioticy.com/' + value, headers=headers)
         check_resp(resp)
     elif element == 'aacc':
+        headers['Authorization'] = users[key.split('-')[1]]
+        resp = requests.delete('http://api.servioticy.com/' + value, headers=headers)
+        check_resp(resp)
+    elif element == 'photocell':
+        headers['Authorization'] = users[key.split('-')[1]]
+        resp = requests.delete('http://api.servioticy.com/' + value, headers=headers)
+        check_resp(resp)
+    elif element == 'tempsensor':
+        headers['Authorization'] = users[key.split('-')[1]]
+        resp = requests.delete('http://api.servioticy.com/' + value, headers=headers)
+        check_resp(resp)
+    elif element == 'humiditysensor':
         headers['Authorization'] = users[key.split('-')[1]]
         resp = requests.delete('http://api.servioticy.com/' + value, headers=headers)
         check_resp(resp)
